d22e24723b653ea83856fe88861758c23cf8f375
kamlesh70 <kamalmehra3571@gmail.com>
2024-05-29 08:50:51 +0530

    recursion basics

--- src/5. recursion/basics.ts
+++ src/5. recursion/basics.ts
@@ -8,6 +8,30 @@ function printNumber(n: number, currentNumber: number): void {
 }
 
 
-export function RecursionBasics(){
-  printNumber(5, 1);
+/*
+ binary search with recursion
+*/ 
+function binarySearch(arr: number[], target: number, start: number, end: number) : number {
+  if(start > end){
+    return -1;
+  }
+  const mid = Math.floor(start + ( end - start )/2);
+  if(arr[mid] === target){
+    return mid;
+  }
+
+  if(arr[mid] < target){
+    return binarySearch(arr, target, mid+1, end);
+  } else {
+    return binarySearch(arr, target, start, mid-1);
+  }
 }
+
+export function RecursionBasics(){
+  const arr = [1,2,3,4,5,6,7,8];
+  const start = 0;
+  const end = arr.length - 1;
+  const ans = binarySearch(arr, 17, start, end);
+  console.log(ans);
+}
+
